8fa12e86558a90e89f0f358fd41ad42f117d1fe2
Irshad1110 <irri1110@gmail.com>
2025-10-05 14:33:22 +0530

    DSA 2.0 pattern 2 and refactor

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 import digitTraversal.InverseOfANumber;
 import digitTraversal.ReverseANumber;
 import digitTraversal.RotateANumber;
+import patterns.RightAngledTriangle;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
+
+public class Main {
+
+    public static void main(String[] args) {
 //    System.out.println( ReverseANumber.reverse(1534236469));
 //    System.out.println( RotateANumber.rotate(1534236, -12));
     System.out.println( InverseOfANumber.inverse(24153));
+        RightAngledTriangle.rightAngledTriangle(5);
+    }
 }
---
+++ src/patterns/RightAngledTriangle.java
@@ -0,0 +1,17 @@
+package patterns;
+
+public class RightAngledTriangle {
+//    *
+//    * *
+//    * * *
+//    * * * *
+//    * * * * *
+
+    public static void rightAngledTriangle(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }System.out.println();
+        }
+    }
+}
